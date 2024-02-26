fc297d4c56251ebfc9acdab0e61521ae20b8cc11
Dmitry Pavlov <dmitrypaulov@MacBook-Pro-Dmitry.local>
2024-02-26 14:50:03 +0300

    add swift lint, formatter

--- Recipes/Recipes/ViewController.swift
+++ Recipes/Recipes/ViewController.swift
@@ -3,9 +3,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
     }
 }
