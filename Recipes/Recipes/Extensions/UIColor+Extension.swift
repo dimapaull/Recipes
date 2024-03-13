7e44b83bdaf72929224a7d664e67ae67ee592cfa
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-13 19:45:15 +0500

    Поправлены прошлые замечания

--- Recipes/Recipes/Extensions/UIColor+Extension.swift
+++ Recipes/Recipes/Extensions/UIColor+Extension.swift
@@ -5,8 +5,14 @@ import UIKit
 
 /// Расширения для цветов UIColor
 extension UIColor {
+    /// Словарь для добавления цветов
     static var colorStoreMap: [String: UIColor] = [:]
     /// Функция проверки цвета в словаре
+    /// - Parameter red: Оттенок красного
+    /// - Parameter green: Оттенок зеленого
+    /// - Parameter blue: Оттенок синего
+    /// - Parameter alpha: Прозрачность
+    /// - Returns: Цвет
     static func rgba(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
         let key = "\(red)\(green)\(blue)\(alpha)"
         if let color = colorStoreMap[key] {
