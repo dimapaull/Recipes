a2db63920f38903309ac06828aa7a25242243df0
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-18 01:59:26 +0500

    Добавлена CoreData полностью

--- Recipes/Recipes/AppDelegate.swift
+++ Recipes/Recipes/AppDelegate.swift
@@ -1,6 +1,7 @@
 // AppDelegate.swift
 // Copyright © RoadMap. All rights reserved.
 
+import CoreData
 import UIKit
 
 @main
@@ -20,5 +21,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         options _: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    /// Создание контейнера (CoreData), который хранит все данные
+    lazy var persistentConteiner: NSPersistentContainer = {
+        let conteiner = NSPersistentContainer(name: "CoreData")
+        conteiner.loadPersistentStores { description, error in
+            if let error {
+                print(error.localizedDescription)
+            } else {
+                print("DB url -", description.url?.absoluteString ?? "Not found url")
+            }
+        }
+        return conteiner
+    }()
+
+    /// Создание слепка - копия контейнера (CoreData) для совершения быстрых действий с БД
+    func saveContext() {
+        let context = persistentConteiner.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                fatalError(error.localizedDescription)
+            }
+        }
     }
 }
